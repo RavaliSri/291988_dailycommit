ce5910636d897368a06c6d2bb8df2550cc4e82af
ravali <ravalisree999@gmail.com>
2021-04-23 14:11:37 +0530

    modified

--- unsolved-dict-2.py
+++
@@ -1,6 +0,0 @@
-num = [1, 2, 3, 4]
-new = current = {}
-for n in num:
-    current[n] = {}
-    current = current[n]
-print(new)
--- unsolved-tuple-1.py
+++
@@ -1,6 +0,0 @@
-t=(6,9,6,8,4,6,9,6)
-print(t.count(6))
-print()
-for i in t:
-    if t.count(i)>1:
-      print(i)
